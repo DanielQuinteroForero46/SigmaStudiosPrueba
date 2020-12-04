9fc0ffeeec9f311ba2caf7f75ad321d5e9b87e9f
DanielQuinteroForero <daquintero27@poligran.edu.co>
2020-12-03 19:40:48 -0500

    Inicialización de proyecto y conexión a base de datos

---
+++ SigmaDB/Repo/Visitante/VisitanteBD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SigmaDB;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PruebaSigma.Models.Repo.Visitante
+{
+    internal class VisitanteBD
+    {
+        
+    }
+}
